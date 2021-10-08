ebd357c1642e497d9d552584f38a97d5edd20b2c
Aaron <kchen339@wisc.edu>
2021-10-08 13:43:59 -0500

    Add mockFileDownloader and startDownload function.

--- app/src/main/java/com/example/lab4_milestone_1/MainActivity.java
+++ app/src/main/java/com/example/lab4_milestone_1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lab4_milestone_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mockFileDownloader(){
+        for (int downloadProgress = 0; downloadProgress <= 100; downloadProgress = downloadProgress + 10) {
+            Log.d(TAG, "Download Progress: " + downloadProgress + "%");
+            try {
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    public void startDownload(View view){
+        mockFileDownloader();
     }
 }
